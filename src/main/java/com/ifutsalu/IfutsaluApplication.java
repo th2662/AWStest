ae8e04684c007ce8f84bf3333d0321ff647a3c1e
woonghyuncho <tofan12312@gmail.com>
2023-05-28 00:57:22 +0900

    feat: Springboot BE project 추가

---
+++ src/main/java/com/ifutsalu/IfutsaluApplication.java
@@ -0,0 +1,13 @@
+package com.ifutsalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfutsaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfutsaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifutsalu/IfutsaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifutsalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfutsaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
