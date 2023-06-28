249e4135aaee3d2f34582604ec24de03bdfcf952
woonghyuncho <tofan12312@gmail.com>
2023-06-29 03:07:10 +0900

    develop 브랜치 conflict 해결

--- src/main/java/com/ifutsalu/IfutsaluApplication.java
+++ src/main/java/com/ifutsalu/IfutsaluApplication.java
@@ -3,8 +3,6 @@ package com.ifutsalu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Slf4j
 @SpringBootApplication
--- src/main/java/com/ifutsalu/service/MatchingService.java
+++ src/main/java/com/ifutsalu/service/MatchingService.java
@@ -5,24 +5,29 @@ import com.ifutsalu.domain.match.MatchingRepository;
 import com.ifutsalu.domain.match.matchParticipation.MatchParticipationRepository;
 import com.ifutsalu.domain.stadium.Stadium;
 import com.ifutsalu.domain.stadium.StadiumRepository;
+import com.ifutsalu.domain.user.Role;
 import com.ifutsalu.domain.user.User;
 import com.ifutsalu.domain.user.UserRepository;
 import com.ifutsalu.dto.request.MatchingRequestDto;
 import com.ifutsalu.dto.response.MatchResponseDto;
+import com.ifutsalu.exception.CustomException;
+import com.ifutsalu.exception.ErrorCode;
 import com.ifutsalu.util.DateTimeFormatterUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class MatchingService {
 
     private final MatchParticipationRepository matchParticipationRepository;
-
     private final MatchingRepository matchingRepository;
 
     private final UserRepository userRepository;
@@ -45,5 +50,29 @@ public class MatchingService {
             return MatchResponseDto.fromEntity(result);
         }
         return MatchResponseDto.builder().build();
+    }
+
+    @Transactional(readOnly = true)
+    public List<MatchResponseDto> getMatchesByUserId(Long userId) {
+        List<Matching> matches = matchParticipationRepository.findMatchesByUserId(userId);
+        List<MatchResponseDto> matchResponseDtos = new ArrayList<>();
+        for (Matching match : matches) {
+            matchResponseDtos.add(MatchResponseDto.fromEntity(match));
+        }
+        return matchResponseDtos;
+    }
+
+    @Transactional(readOnly = true)
+    public List<MatchResponseDto> getMatchesByManager(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_USER));
+        if (user.getRole() != Role.ROLE_MANAGER) {
+            throw new CustomException(ErrorCode.FORBIDDEN_MEMBER);
+        }
+
+        List<Matching> matches = matchingRepository.findByManager(user);
+        return matches.stream()
+                .map(MatchResponseDto::fromEntity)
+                .collect(Collectors.toList());
     }
 }
